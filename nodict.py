9656d3341ad7358d361be558034d33c7a61def9e
Michael DeMory <mjdemory2891@gmail.com>
2020-07-22 13:43:46 -0400

    ran tests on node, passed all tests

--- nodict.py
+++ nodict.py
@@ -8,19 +8,22 @@ __author__ = '???'
 
 class Node:
     def __init__(self, key, value=None):
-        self.hash = None
-        self.key = None
-        self.value = None
+        self.hash = hash(key)
+        self.key = key
+        self.value = value
         # Your code here
         return
 
     def __repr__(self):
         # Your code here
-        return
+        return f'{self.__class__.__name__}({self.key}, {self.value})'
 
     def __eq__(self, other):
         # Your code here
-        return
+        if isinstance(self, other.__class__):
+            return self.key == other.key
+        else:
+            return NotImplemented
 
 
 class NoDict:
