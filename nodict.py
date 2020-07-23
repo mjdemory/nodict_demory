02b9bcb0af1d2dfecaba2543ed96f9bbc2ca8ae9
Michael DeMory <mjdemory2891@gmail.com>
2020-07-23 15:28:28 -0400

    pass all tests

--- nodict.py
+++ nodict.py
@@ -3,11 +3,13 @@
 Implementation of the NoDict assignment
 """
 
-__author__ = 'Michael DeMory'
+__author__ = """Michael DeMory and helped by
+                Tiffany McLean, Zac Gerber, JT, Peiro."""
 
 
 class Node:
     def __init__(self, key, value=None):
+        """Initilize data members of class when object is created"""
         self.hash = hash(key)
         self.key = key
         self.value = value
@@ -15,10 +17,12 @@ class Node:
         return
 
     def __repr__(self):
+        """Makes the object readable"""
         # Your code here
         return f'{self.__class__.__name__}({self.key}, {self.value})'
 
     def __eq__(self, other):
+        """Compares object to other"""
         # Your code here
         if isinstance(self, other.__class__):
             return self.key == other.key
@@ -28,29 +32,50 @@ class Node:
 
 class NoDict:
     def __init__(self, num_buckets=10):
-        self.buckets = [ [] for _ in range(num_buckets) ]
-        self.bucket_list = self.buckets
+        """Initilize data members of class when object is created"""
+        self.buckets = [[] for _ in range(num_buckets)]
         # Your code here
+        self.num = num_buckets
 
     def __repr__(self):
+        """Makes the object readable"""
         # Your code here
-        return '\n'.join([f'{self.__class__.__name__}.{i}:{bucket}' for i, bucket in enumerate(self.buckets)])
+        return '\n'.join([f'{self.__class__.__name__}.{i}:{bucket}'
+                         for i, bucket in enumerate(self.buckets)])
 
-    def add(self, key, value):
+    def add(self, key, value=None):
+        """Adds new object to bucket"""
         new_node = Node(key, value)
-        bucket_index = hash(new_node) % self.buckets
-        # Stuck here. Not sure if this is how to attain index of bucket.
-        
-        return
+
+        bucket_index = self.buckets[new_node.hash % self.num]
+
+        for i in bucket_index:
+            if i == new_node:
+                bucket_index.remove(i)
+                break
+        bucket_index.append(new_node)
+
+        # Your code here
 
     def get(self, key):
+        """Looks up key"""
+        node_to_find = Node(key)
+
+        bucket_index = self.buckets[node_to_find.hash % self.num]
+
+        for i in bucket_index:
+            if i == node_to_find:
+                return i.value
+        raise KeyError(f'{key} not found')
+
         # Your code here
-        return
 
     def __getitem__(self, key):
+        """Uses get function"""
         # Your code here
-        return
+        return self.get(key)
 
     def __setitem__(self, key, value):
+        """Uses add function"""
         # Your code here
-        return
+        return self.add(key, value)
